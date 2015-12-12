c30619d095c114e382b931751fca4f5a129844b7
Eric Tingey <ebtingey@gmail.com>
2015-12-12 11:55:37 -0700

    Naming changes in code, mainly changing e,w,a,i to loc,spr,skw,scl

--- vorticity.f90
+++ vorticity.f90
@@ -1,15 +1,15 @@
-!Subroutine to create the shape and strength distribution of the vorticity in a VAWT wake
-!To be used with VAWT Wake Model.py
+! Subroutine to create the shape and strength distribution of the vorticity in a VAWT wake
+! To be used with VAWT_Wake_Model.py
 
 
-!Creating the fit of the vorticity distribution
-subroutine skew(y,e,w,a,i,gam_skew)
+! Creating the fit of the vorticity distribution
+subroutine dist(y,loc,spr,skw,scl,gam_skew)
     implicit none
 
     integer, parameter :: dp = kind(0.d0)
 
     ! in
-    real(dp), intent(in) :: y,e,w,a,i
+    real(dp), intent(in) :: y,loc,spr,skw,scl
 
     ! out
     real(dp), intent(out) :: gam_skew
@@ -21,78 +21,76 @@ subroutine skew(y,e,w,a,i,gam_skew)
     intrinsic sqrt
 
     ! Exponentially Modified Gaussian Distribution
-    gam_skew = i*a/2.0_dp*exp(a/2.0_dp*(2.0_dp*e+a*w**2.0_dp-2.0_dp*y))*(1.0_dp-erf((e + a*w**2.0_dp -y)/(sqrt(2.0_dp)*w)))
+    gam_skew = scl*skw/2.0_dp*exp(skw/2.0_dp*(2.0_dp*loc+skw*spr**2.0_dp-2.0_dp*y))*(1.0_dp-erf((loc + skw*spr**2.0_dp -y)/(sqrt(2.0_dp)*spr)))
 
-end subroutine skew
+end subroutine dist
 
 
-!Calculating vorticity strength in the s and t directions
-subroutine gamma(x,y,dia,e1,e2,e3,w1,w2,a1,a2,i1,i2,i3,gam_lat)
+! Calculating vorticity strength in the x and y directions
+subroutine gamma(x,y,dia,loc1,loc2,loc3,spr1,spr2,skw1,skw2,scl1,scl2,scl3,gam_lat)
     implicit none
 
     integer, parameter :: dp = kind(0.d0)
 
     ! in
     real(dp), intent(in) :: x,y,dia
-    real(dp), intent(in) :: e1,e2,e3,w1,w2,a1,a2
-    real(dp), intent(in) :: i1,i2,i3
+    real(dp), intent(in) :: loc1,loc2,loc3,spr1,spr2,skw1,skw2,scl1,scl2,scl3
 
     ! out
     real(dp), intent(out) :: gam_lat
 
     ! local
-    real(dp) :: xd,yd,e,w,a,i,g1,g2,gam
+    real(dp) :: xd,yd,loc,spr,skw,scl,g1,g2,gam
     intrinsic exp
 
-    xd = x/dia ! normalizing s by the diameter
-    yd = y/dia ! normalizing t by the diameter
+    xd = x/dia ! normalizing x by the diameter
+    yd = y/dia ! normalizing y by the diameter
 
-    e = e1*xd**2 + e2*xd + e3
-    w = w1*xd + w2
-    a = a1*xd + a2
-    i = i1/(1.0_dp + exp(i2*(xd - i3)))
+    loc = loc1*xd**2 + loc2*xd + loc3
+    spr = spr1*xd + spr2
+    skw = skw1*xd + skw2
+    scl = scl1/(1.0_dp + exp(scl2*(xd - scl3)))
 
     ! Limiting the fits to maximum values the Weibull distribution can handle
-    if (e < 0.0_dp) then
-        e = 0.0_dp
+    if (loc < 0.0_dp) then
+        loc = 0.0_dp
     end if
-    if (a > 0.0_dp) then
-       a = 0.0_dp
+    if (skw > 0.0_dp) then
+       skw = 0.0_dp
     end if
 
+    call dist(yd,loc,spr,skw,scl,g1)
+    call dist(yd,-loc,-spr,-skw,-scl,g2)
 
-    call skew(yd,e,w,a,i,g1)
-    call skew(yd,-e,-w,-a,-i,g2)
-
-    gam = (g1 - g2)
-
-    gam_lat = gam ! readjusting the vorticity strength to remove normalization
+    gam_lat = (g1 - g2)
 
 end subroutine gamma
 
-!Creating the integral for VAWT Wake Model.py
-subroutine integrand(y,x,x0,y0,dia,e1,e2,e3,w1,w2,a1,a2,i1,i2,i3,inte)
+! Creating the integral for VAWT Wake Model.py
+subroutine integrand(y,x,x0,y0,dia,loc1,loc2,loc3,spr1,spr2,skw1,skw2,scl1,scl2,scl3,inte)
     implicit none
 
     integer, parameter :: dp = kind(0.d0)
 
     ! in
     real(dp), intent(in) :: y,x,x0,y0,dia
-    real(dp), intent(in) :: e1,e2,e3,w1,w2,a1,a2
-    real(dp), intent(in) :: i1,i2,i3
+    real(dp), intent(in) :: loc1,loc2,loc3,spr1,spr2,skw1,skw2,scl1,scl2,scl3
 
     ! out
     real(dp), intent(out) :: inte
 
     ! local
-    real(dp) :: gammav
+    real(dp) :: xd,yd,x0d,y0dgammav
 
+    ! Specifying the strength of the vorticity
+    call gamma(x,y,dia,loc1,loc2,loc3,spr1,spr2,skw1,skw2,scl1,scl2,scl3,gammav)
+    
+    xd = x/dia ! normalizing x by the diameter
+    yd = y/dia ! normalizing y by the diameter
+    x0d = x0/dia ! normalizing x0 by the diameter
+    y0d = y0/dia ! normalizing y0 by the diameter
 
-    !Specifying the strength of the vorticity
-    call gamma(x,y,dia,e1,e2,e3,w1,w2,a1,a2,i1,i2,i3,gammav)
-
-    ! inte = gammav*((td - yd)/((xd - xd)**2 + (td - yd)**2))
-    inte = gammav*((y - y0)/((x - x0)**2 + (y - y0)**2))
+    inte = gammav*((yd - y0d)/((xd - x0d)**2 + (yd - y0d)**2))
 
 end subroutine integrand
 
