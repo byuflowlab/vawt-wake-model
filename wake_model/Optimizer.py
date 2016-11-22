Renaming files, moving code (cleaning up), adding airfoil data, adjusting error calculation on validation studies, correcting AC code, updating optimization code

wake_model/Optimizer.py
@@ -1,37 +1,65 @@
 from pyoptsparse import Optimization, SNOPT, pyOpt_solution
+from os import path
 import numpy as np
 from numpy import sqrt
 import matplotlib.pyplot as plt
+import VAWT_Wake_Model as vwm
+from AC_InducedVel import induced_vel
 
-from Wind_Farm_Layout import overlap,powerval
+import _vawtwake
 
 
 def obj_func(xdict):
     global dia
-    global tsr
-    global solidity
-    global Cp
-    global dens
+    global rot
+    global chord
+    global twist
+    global delta
+    global B
+    global H
+    global rho
+    global mu
     global velf
+
+    global af_data
+    global cl_data
+    global cd_data
+    global coef0
+    global coef1
+    global coef2
+    global coef3
+    global coef4
+    global coef5
+    global coef6
+    global coef7
+    global coef8
+    global coef9
+
     global funcs
+
+    ntheta = 36
     
     Vars = xdict['xvars']
     x = xdict['xvars']
     y = xdict['yvars']
     funcs = {}
-    
-    veleff = np.zeros_like(x)
+
     power_turb = np.zeros_like(x)
-    
+
     for i in range(np.size(x)):
-        xp = np.delete(x,i)
-        yp = np.delete(y,i)
-        diap = np.delete(dia,i)
-        tsrp = np.delete(tsr,i)
-        solidityp = np.delete(solidity,i)
-        veleff[i] = overlap(xp,yp,diap,tsrp,solidityp,x[i],y[i],dia[i],velf,True,False)
-        power_turb[i] = powerval(Cp,dens,veleff[i],dia[i])
-        # print i+1
+        xt = np.delete(x,i)
+        yt = np.delete(y,i)
+        diat = np.delete(dia,i)
+        rott = np.delete(rot,i)
+        xt = np.insert(xt,0,x[i])
+        yt = np.insert(yt,0,y[i])
+        diat = np.insert(diat,0,dia[i])
+        rott = np.insert(rott,0,rot[i])
+
+        velx,vely = induced_vel(dia[i]/2.,af_data,cl_data,cd_data,chord,twist,delta,B,rot[i],velf,rho,mu,ntheta)
+
+        power_turb[i],_ = _vawtwake.powercalc(xt,yt,diat,rott,velf,coef0,coef1,coef2,coef3,coef4,coef5,coef6,coef7,coef8,coef9,af_data,cl_data,cd_data,chord,twist,delta,B,H,rho,mu,velx,vely)
+        print i+1
     
     power = np.sum(power_turb)
     print power 
@@ -39,7 +67,6 @@ def obj_func(xdict):
     
     sep = sep_func(np.append(x,y))
     funcs['sep'] = sep
-    funcs['veleff'] = veleff
     funcs['power_turb'] = power_turb
     
     fail = False
@@ -73,21 +100,44 @@ if __name__ == "__main__":
     contour = True
     contour = False
     
-    
     global dia
-    global tsr
-    global solidity
-    global Cp
-    global dens
+    global rot
+    global chord
+    global twist
+    global delta
+    global B
+    global H
+    global rho
+    global mu
     global velf
-    global turb_dia
+
+    global af_data
+    global cl_data
+    global cd_data
+    global coef0
+    global coef1
+    global coef2
+    global coef3
+    global coef4
+    global coef5
+    global coef6
+    global coef7
+    global coef8
+    global coef9
+
     global funcs
+
+    foildata = '/Users/ning1/Documents/FLOW Lab/VAWTAC/airfoils/NACA_0021.dat'
+
+    af_data,cl_data,cd_data = vwm.airfoil_data(foildata)
+    coef0,coef1,coef2,coef3,coef4,coef5,coef6,coef7,coef8,coef9 = vwm.coef_val()
     
     # define turbine size
     turb_dia = 6. # m
+    turb_rot = 20. # rad/s
     
     # Scaling grid case
-    nRows = 3     # number of rows and columns in grid
+    nRows = 2     # number of rows and columns in grid
     spacing = 5     # turbine grid spacing in diameters
     
     # Set up position arrays
@@ -96,14 +146,21 @@ if __name__ == "__main__":
     xt = np.ndarray.flatten(xpoints)
     yt = np.ndarray.flatten(ypoints)
     dia = np.ones_like(xt)*turb_dia
-    tsr = np.ones_like(xt)*4.0
-    solidity = np.ones_like(xt)*0.25
-    
-    Cp = 0.4
-    dens = 1.225
+    rot = np.ones_like(xt)*turb_rot
+
+    twist = 0.0
+    delta = 0.0
+    B = 3
+    chord = 0.25
+    H = 5.
+
+    rho = 1.225
+    mu = 1.7894e-5
     velf = 15.
-    
-    power_iso = powerval(Cp,dens,velf,turb_dia)
+
+    ntheta = 36
+    velx,vely = induced_vel(dia[0]/2.,af_data,cl_data,cd_data,chord,twist,delta,B,rot[0],velf,rho,mu,ntheta)
+    power_iso,_ = _vawtwake.powercalc(np.array([0.]),np.array([0.]),np.array([turb_dia]),np.array([turb_rot]),velf,coef0,coef1,coef2,coef3,coef4,coef5,coef6,coef7,coef8,coef9,af_data,cl_data,cd_data,chord,twist,delta,B,H,rho,mu,velx,vely)
     
     x0 = xt
     y0 = yt
@@ -132,15 +189,13 @@ if __name__ == "__main__":
     xf = res.xStar['xvars']
     yf = res.xStar['yvars']
     power_turb = funcs['power_turb']
-    veleff = funcs['veleff']
     
-    print 'The power is:',pow,'kJ'
-    print 'The isolated power is:',power_iso*np.size(xt),'kJ'
+    print 'The power is:',pow,'W'
+    print 'The isolated power is:',power_iso*np.size(xt),'W'
     print 'The x-locations:',xf
     print 'The y-locations:',yf
-    print 'The effective wind speeds:',veleff
-    print 'The power of each turbine (kJ):',power_turb
-    print 'The isolated power of one turbine is:',power_iso,'kJ'
+    print 'The power of each turbine (W):',power_turb
+    print 'The isolated power of one turbine is:',power_iso,'W'
     
     
     plt.figure(1)
@@ -167,7 +222,8 @@ if __name__ == "__main__":
         k = 0
         for i in range(N):
             for j in range(N):
-                Vel[i,j] = overlap(xf,yf,dia,tsr,solidity,X[i,j],Y[i,j],turb_dia,velf,True,False)
+                Velx,Vely = _vawtwake.overlappoint(xt,yt,dia,rot,chord,B,X[i,j],Y[i,j],velf,coef0,coef1,coef2,coef3,coef4,coef5,coef6,coef7,coef8,coef9,220,200,1)
+                Vel[i,j] = sqrt((Velx+velf)**2 + (Vely)**2)
                 k += 1
                 print k
         

wake_model/VAWT_Wake_Model.py
@@ -3,10 +3,10 @@ Parameterized VAWT Wake Model using CFD vorticity data
 Developed by Eric Tingey at Brigham Young University
 
 This code models the wake behind a vertical-axis wind turbine based on
-parameters like tip-speed ratio, solidity and wind speed by converting the
-vorticity of the wake into velocity information. The model uses CFD data
-obtained from STAR-CCM+ of simulated turbines to make the wake model as
-accurate as possible.
+tip-speed ratio, solidity and wind speed by converting the vorticity of
+the wake into velocity information. The model uses CFD data obtained
+from STAR-CCM+ of simulated turbines to make the wake model as accurate
+as possible.
 
 Only valid for tip-speed ratios between 1.5 and 7.0 and solidities between
 0.15 and 1.0. Reynolds numbers should also be around the range of 600,000 to
@@ -52,6 +52,44 @@ def _parameterval(tsr,sol,coef):
     j = coef[9]
 
     return a + b*tsr + c*sol + d*tsr**2 + e*tsr*sol + f*sol**2 + g*tsr**3 + h*tsr**2*sol + i*tsr*sol**2 + j*sol**3
+
+
+def coef_val():
+    # Published coefficients (read 4: 1 2 2 1 1 1 2 1 2 2)
+    coef0 = np.array( [0.0025703809856661534, -0.0007386258659065129, 0.004595508188667984, 0.000380123563204793, -0.0005090098755683027, 0.005744581813281894, -4.103393770815313e-05, -0.0014146918534486358, -0.013975958482495927, 0.0] )
+    coef1 = np.array( [-0.5047504670963536, 0.23477391362058556, 0.8414256436198028, -0.04252528528617351, -0.06962875967504166, -0.6566907653208429, 0.002839318332370807, 0.00571803958194812, 0.0070744372783060295, 0.22805286438890995] )
+    coef2 = np.array( [0.2878345841026334, 0.11512552658662782, 0.7303949879914625, -0.007035517839387948, -0.18284850673545897, -0.5241921153256568, -0.0003704899921255296, 0.010972527139685873, 0.04380801537377295, 0.1724129349605399] )
+    coef3 = np.array( [0.08234816067475287, -0.03530687906626052, -0.3662863944976986, 0.003240141344532779, 0.12172015102204112, 0.2993048183466721, 0.0, -0.009253185586804007, -0.057469126406649716, -0.07257633583877886] )
+    coef4 = np.array( [-0.07083579909945328, 0.016182024377569406, 0.1985436342461859, 0.0017738254727425816, -0.09111094817943823, -0.06561408122153217, -0.0005115133402638633, 0.009434288536679505, 0.022392136905926813, 0.0] )
+    coef5 = np.array( [-1.6712830849073221, 1.5625053380692426, -6.180392756736983, -0.20407668040293722, -4.6476103643607685, 29.380064536220306, 0.0, 0.7502978877582536, -0.16358232641365608, -19.937609244085568] )
+    coef6 = np.array( [-3.423561091777921, -9.228795430171687, 86.95722105482042, 2.772872601988039, -11.968168333741515, -150.61261090270446, -0.24715316589674527, 0.5283723108899993, 4.537286811245538, 82.50581844010263] )
+    coef7 = np.array( [-0.19815381951708524, 0.08438758133540872, 1.2650146439483734, -0.007606115512168328, -0.2747023984740461, -0.8844640101378567, 0.0, 0.01870057580949183, 0.0699898278743648, 0.2794360008051127] )
+    coef8 = np.array( [2.3932787625531815, -2.020874419612962, -8.938221963838357, 0.576323845480877, 2.8782448498416944, 16.598492450314534, -0.04746016700352029, -0.197101203594028, -1.3860007472886064, -8.289767128060362] )
+    coef9 = np.array( [104.40501489600803, -29.942999569370276, -174.42008279158216, 3.708514822202037, 25.14336546356742, 132.35546551746415, -0.16479555172343271, -1.351556690339512, -6.721810844025761, -40.39565289044579] )
+
+    return coef0,coef1,coef2,coef3,coef4,coef5,coef6,coef7,coef8,coef9
+
+
+def airfoil_data(file):
+    af_data = np.array([])
+    cl_data = np.array([])
+    cd_data = np.array([])
+
+    f = open(file, 'r')
+    for i in range(13):
+        f.readline()
+    for line in f:
+        line = line.strip()
+        columns = line.split()
+        if columns[0] == 'EOT':
+            break
+        else:
+            af_data = np.append(af_data,float(columns[0]))
+            cl_data = np.append(cl_data,float(columns[1]))
+            cd_data = np.append(cd_data,float(columns[2]))
+    f.close()
+
+    return af_data,cl_data,cd_data
 
 
 def velocity_field(xt,yt,x0,y0,velf,dia,rot,chord,B,param=None,veltype='all',integration='simp',m=220,n=200):
@@ -102,16 +140,7 @@ def velocity_field(xt,yt,x0,y0,velf,dia,rot,chord,B,param=None,veltype='all',int
     y0t = y0 - yt
 
     # Published coefficients (read 4: 1 2 2 1 1 1 2 1 2 2)
-    coef0 = np.array( [0.0025703809856661534, -0.0007386258659065129, 0.004595508188667984, 0.000380123563204793, -0.0005090098755683027, 0.005744581813281894, -4.103393770815313e-05, -0.0014146918534486358, -0.013975958482495927, 0.0] )
-    coef1 = np.array( [-0.5047504670963536, 0.23477391362058556, 0.8414256436198028, -0.04252528528617351, -0.06962875967504166, -0.6566907653208429, 0.002839318332370807, 0.00571803958194812, 0.0070744372783060295, 0.22805286438890995] )
-    coef2 = np.array( [0.2878345841026334, 0.11512552658662782, 0.7303949879914625, -0.007035517839387948, -0.18284850673545897, -0.5241921153256568, -0.0003704899921255296, 0.010972527139685873, 0.04380801537377295, 0.1724129349605399] )
-    coef3 = np.array( [0.08234816067475287, -0.03530687906626052, -0.3662863944976986, 0.003240141344532779, 0.12172015102204112, 0.2993048183466721, 0.0, -0.009253185586804007, -0.057469126406649716, -0.07257633583877886] )
-    coef4 = np.array( [-0.07083579909945328, 0.016182024377569406, 0.1985436342461859, 0.0017738254727425816, -0.09111094817943823, -0.06561408122153217, -0.0005115133402638633, 0.009434288536679505, 0.022392136905926813, 0.0] )
-    coef5 = np.array( [-1.6712830849073221, 1.5625053380692426, -6.180392756736983, -0.20407668040293722, -4.6476103643607685, 29.380064536220306, 0.0, 0.7502978877582536, -0.16358232641365608, -19.937609244085568] )
-    coef6 = np.array( [-3.423561091777921, -9.228795430171687, 86.95722105482042, 2.772872601988039, -11.968168333741515, -150.61261090270446, -0.24715316589674527, 0.5283723108899993, 4.537286811245538, 82.50581844010263] )
-    coef7 = np.array( [-0.19815381951708524, 0.08438758133540872, 1.2650146439483734, -0.007606115512168328, -0.2747023984740461, -0.8844640101378567, 0.0, 0.01870057580949183, 0.0699898278743648, 0.2794360008051127] )
-    coef8 = np.array( [2.3932787625531815, -2.020874419612962, -8.938221963838357, 0.576323845480877, 2.8782448498416944, 16.598492450314534, -0.04746016700352029, -0.197101203594028, -1.3860007472886064, -8.289767128060362] )
-    coef9 = np.array( [104.40501489600803, -29.942999569370276, -174.42008279158216, 3.708514822202037, 25.14336546356742, 132.35546551746415, -0.16479555172343271, -1.351556690339512, -6.721810844025761, -40.39565289044579] )
+    coef0,coef1,coef2,coef3,coef4,coef5,coef6,coef7,coef8,coef9 = coef_val()
 
     # Calculating EMG distribution parameters (looking up or reading in)
     if param is None:
@@ -154,7 +183,7 @@ def velocity_field(xt,yt,x0,y0,velf,dia,rot,chord,B,param=None,veltype='all',int
             inte = 1 # Simpson's Rule
             # inte = 2 # Trapezoidal Rule (optional ability of the code-- faster but less accurate)
 
-            vel_xs,vel_ys = _vawtwake.vel_field(xt,yt,x0,y0,dia,rot,chord,B,coef0,coef1,coef2,coef3,coef4,coef5,coef6,coef7,coef8,coef9,velf,m,n,inte)
+            vel_xs,vel_ys = _vawtwake.vel_field(xt,yt,x0,y0,dia,rot,chord,B,velf,coef0,coef1,coef2,coef3,coef4,coef5,coef6,coef7,coef8,coef9,m,n,inte)
 
             if veltype == 'all':
                 vel = sqrt((vel_xs*velf + velf)**2 + (vel_ys*velf)**2)/velf
@@ -168,9 +197,11 @@ def velocity_field(xt,yt,x0,y0,velf,dia,rot,chord,B,param=None,veltype='all',int
         elif integration == 'gskr':
             # 21 POINT GAUSS-KRONROD RULE QUADRATURE INTEGRATION
             xbound = (scl3+5.)*dia
+            # xbound = 45.*dia/max(1, int(tsr*solidity))
             argval = (x0t,y0t,dia,loc1,loc2,loc3,spr1,spr2,skw1,skw2,scl1,scl2,scl3)
             if veltype == 'all' or veltype == 'x' or veltype == 'ind':
                 vel_x = dblquad(_vawtwake.integrandx,0.,xbound,lambda x: -1.*dia,lambda x: 1.*dia,args=argval)
+                # vel_x = dblquad(_vawtwake.integrandx,0.,xbound,lambda x: -4.*dia,lambda x: 4.*dia,args=argval)
                 vel_xs = (vel_x[0]*rot)/(2.*pi)
             if veltype == 'all' or veltype == 'y' or veltype == 'ind':
                 vel_y = dblquad(_vawtwake.integrandy,0.,xbound,lambda x: -1.*dia,lambda x: 1.*dia,args=argval)

wake_model/Velocity_Example.py
@@ -7,7 +7,7 @@ import time
 
 start = time.time()
 
-# Option to plot velocity profiles
+# Option to plot velocity profiles at 2D, 4D, 6D, 8D, 10D, and 15D downstream
 vel_slice = True
 vel_slice = False # comment this out if desired on
 
@@ -16,31 +16,32 @@ plot_dist = True
 # plot_dist = False # comment this out if desired on
 
 # Enter the values desired
-
-velf = 15.0 # free stream wind speed (m/s)
-dia = 6.  # turbine diameter (m)
-tsr = 4.  # tip speed ratio
-B = 3. # number of blades
-chord = 0.25 # chord lenth (m)
-rot = tsr*velf/(dia/2.)
+velf = 15.0                 # free stream wind speed (m/s)
+dia = 6.                    # turbine diameter (m)
+tsr = 4.                    # tip speed ratio ((dia/2)*rot/velf)
+B = 3.                      # number of blades
+chord = 0.25                # chord lenth (m)
+rot = tsr*velf/(dia/2.)     # rotation rate (rad/s)
 
 # Enter the positions of the turbine and velocity calculation
-xt = 0. # downstream position of turbine in flow domain (m)
-yt = 0. # later position of turbine in flow domain (m)
-x0 = 24. # downstream distance from turbine for velocity calculation (m)
-y0 = 0. # lateral distance from turbine for velocity calculation (m)
+xt = 0.         # downstream position of turbine in flow domain (m)
+yt = 0.         # later position of turbine in flow domain (m)
+x0 = 24.        # downstream distance for velocity calculation (m)
+y0 = 0.         # lateral distance for velocity calculation (m)
 
-veltype = 'vort'
+# Option to choose the type of velocity calculations to make
+veltype = 'vort'        # calculate only vorticity
 
-veltype = 'all'
-# veltype = 'x'
-# veltype = 'y'
-# veltype = 'ind'
+veltype = 'all'         # calculate velocity magnitude
+veltype = 'x'         # calculate x-velocity
+# veltype = 'y'         # calcuate y-velocity
+# veltype = 'ind'       # calculate induced velocity (in both x and y directions)
 
-integration = 'simp'
-m = 220
-n = 200
-# integration = 'gskr'
+# Option to choose the method of integration
+integration = 'simp'    # use Simpson's Rule integration (Fortran code)
+m = 220                 # number of divisions in the downstream direction (for Simpson's Rule)
+n = 200                 # number of divisions in the lateral direction (for Simpson's Rule)
+# integration = 'gskr'  # use 21 Point Gauss-Kronrod Rule Quadrature integration
 
 ########################################################################################################################
 ########################################################################################################################
@@ -54,9 +55,9 @@ pointval2 = 0.
 pointval3 = 0.
 
 ## Plotting
-fs = 25 # 18# font size for plots
+fs = 25 # font size for plots
 
-# Plotting velocity profiles
+# PLOTTING VELOCITY PROFILES
 if vel_slice == True:
     leng = 100. # data points in the velocity profile
     wide = 2.0*dia # width of the profile
@@ -95,7 +96,7 @@ if vel_slice == True:
     plt.xlabel(r'$u/U_\infty$', fontsize=fs)
     plt.ylabel('$y/D$',fontsize=fs)
 
-# Plotting full velocity domain
+# PLOTTING FULL VELOCITY DOMAIN
 if plot_dist == True:
     xi = -3.*dia # starting point in downstream direction
     xf = 17.0*dia # ending point in downstream direction
@@ -111,32 +112,33 @@ if plot_dist == True:
     VEL = np.zeros((N, N)) # initiallizing velocity data point array
     VELy = np.zeros((N, N))
 
+    x_check = np.zeros(N*N)
+    y_check = np.zeros(N*N)
+    vel_check = np.zeros(N*N)
+
     iter = 0
     for i in range(N):
         for j in range(N):
             if veltype == 'all' or veltype == 'x' or veltype == 'y' or veltype == 'velfort':
                 VEL[i,j] = velocity_field(xt,yt,X[i,j],Y[i,j],velf,dia,rot,chord,B,param=None,veltype=veltype,integration=integration,m=m,n=n)
+                x_check[iter] = xp[i]
+                y_check[iter] = yp[j]
+                vel_check[iter] = velocity_field(xt,yt,xp[i],yp[j],velf,dia,rot,chord,B,param=None,veltype=veltype,integration=integration,m=m,n=n)
             elif veltype == 'ind':
                 velfd = velocity_field(xt,yt,X[i,j],Y[i,j],velf,dia,rot,chord,B,param=None,veltype=veltype,integration=integration,m=m,n=n)
                 VEL[i,j] = velfd[0]
                 VELy[i,j] = velfd[1]
             elif veltype == 'vort':
                 VEL[i,j] = velocity_field(xt,yt,X[i,j],Y[i,j],velf,dia,rot,chord,B,param=None,veltype=veltype,integration=integration,m=m,n=n)
-            iter = iter +1
+            iter += 1
             print 'Plot ('+str(iter)+' of '+str(N*N)+')'
 
-    if veltype == 'all' or veltype == 'x' or veltype == 'y' or veltype == 'velfort':
+    if veltype == 'all' or veltype == 'x' or veltype == 'y':
         fig = plt.figure(2,figsize=(19,5))
         fig.subplots_adjust(bottom=.16,left=.05,right=1.0)
-        if veltype == 'all' or veltype == 'x' or veltype == 'velfort':
+        if veltype == 'all' or veltype == 'x':
             lb = 0.15 # lower bound on velocity to display
             ub = 1.15 # upper bound on velocity to display
-
-            # lb = -0.75 # lower bound on velocity to display ind x-vel
-            # ub = 0.4 # upper bound on velocity to display ind x-vel
-            # lb = 0.0 # lower bound on velocity to display ind x-vel
-            # ub = 25./15. # upper bound on velocity to display ind x-vel
-
         elif veltype == 'y':
             lb = -0.35 # lower bound on velocity to display
             ub = 0.35 # upper bound on velocity to display
@@ -145,14 +147,14 @@ if plot_dist == True:
         v = np.linspace(lb,ub,6) # setting the number of tick marks on colorbar
         CS = plt.contourf(X/dia,Y/dia,VEL,ran,vmax=ub,vmin=lb,levels=bounds,cmap=plt.cm.coolwarm) # plotting the contour plot
         CB = plt.colorbar(CS, ticks=v) # creating colorbar
-        CB.ax.set_ylabel(r'$u/U_\infty$',fontsize=fs)
+        if veltype == 'y':
+            CB.ax.set_ylabel(r'$v/U_\infty$',fontsize=fs)
+        else:
+            CB.ax.set_ylabel(r'$u/U_\infty$',fontsize=fs)
         CB.ax.tick_params(labelsize=fs)
         CB.ax.set_aspect(20)
     elif veltype == 'ind':
         fig = plt.figure(2,figsize=(19,5))
-        # fig.subplots_adjust(bottom=.16,left=.05,right=0.81)
-        # CS = plt.quiver(X/dia,Y/dia, VEL, VELy)
-        # plt.quiverkey(Q,1.1,0.8,1,'1 m/s',fontproperties={'size':fs})
         fig.subplots_adjust(bottom=.16,left=.05,right=1.0)
         speed = np.sqrt(VEL*VEL + VELy*VELy)
         CS = plt.streamplot(X/dia, Y/dia, VEL, VELy, density=2, color=speed, cmap=plt.cm.coolwarm)
@@ -181,20 +183,6 @@ if plot_dist == True:
     plt.ylim(yd/dia,yu/dia)
     circ = plt.Circle((xt/dia,yt/dia),0.5,edgecolor='k',fill=False)
     plt.gca().add_patch(circ)
-
-
-    # if veltype == 'all':
-    #     plt.savefig('/Users/ning1/Documents/FLOW Lab/mag-vel.png')
-    # elif veltype == 'x':
-    #     plt.savefig('/Users/ning1/Documents/FLOW Lab/x-vel_newplot.png')
-    # elif veltype == 'y':
-    #     plt.savefig('/Users/ning1/Documents/FLOW Lab/y-vel.png')
-    # elif veltype == 'ind':
-    #     plt.savefig('/Users/ning1/Documents/FLOW Lab/ind-vel.png')
-    # elif veltype == 'vort':
-    #     plt.savefig('/Users/ning1/Documents/FLOW Lab/vort-plot.png')
-    # elif veltype == 'velfort':
-    #     plt.savefig('/Users/ning1/Documents/FLOW Lab/mag-velfort_xind.png')
 
 sec = time.time()-start
 pointval = pointval1+pointval2+pointval3

wake_model/validation/battisti_val.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
 from VAWT_Wake_Model import velocity_field
-from database_call import vorticity,vorticity2,velocity,velocity2
 from numpy import fabs
 
 from matplotlib import rcParams
@@ -12,6 +10,10 @@ r = 0.5 # radius
 v = 1.0 # velocity
 
 veltype = 'x'
+errortype = 'abs'
+errortype = 'rel'
+epsilon = 1e-3
+errortype = 'rms'
 
 x15 = np.linspace(-1.7,1.7,21)
 x15r = np.linspace(-1.75,1.75,100)
@@ -34,10 +36,24 @@ error_test = np.zeros_like(x15)
 
 for i in range(np.size(x15)):
     rom15t[i] = velocity_field(0.,0.,1.5*dia,x15[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
-    error_test[i] = (rom15t[i]-y15o[i])/y15o[i]
+
+    if errortype == 'abs':
+        error_test[i] = fabs((1.-rom15t[i])-(1.-y15o[i]))
+    elif errortype == 'rel':
+        if fabs(1.-y15o[i]) >= epsilon:
+            error_test[i] = fabs(((1.-rom15t[i])-(1.-y15o[i]))/(1.-y15o[i]))
+        else:
+            error_test[i] = fabs((1.-rom15t[i])-(1.-y15o[i]))
+    elif errortype == 'rms':
+        error_test[i] = ((1.-rom15t[i])-(1.-y15o[i]))**2.
     print 'error calc',i+1,'of',np.size(x15)
-error = np.average(fabs(error_test))
-errorstd = np.std(fabs(error_test))
+
+if errortype == 'abs' or errortype == 'rel':
+    error = np.average(fabs(error_test))
+    errorstd = np.std(fabs(error_test))
+elif errortype == 'rms':
+    error = np.sqrt(np.average(error_test))
+    errorstd = 1.
 for i in range(np.size(rom15)):
     rom15[i] = velocity_field(0.,0.,1.5*dia,x15r[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
     print 'plot point',i+1,'of',np.size(x15r)
@@ -59,9 +75,22 @@ plt.legend(loc=1,fontsize=fs)
 # plt.text(-0.5,0.9,'X/D = 1.5')
 # print '1.5 modc',(min(rom15)-min(y15c))/min(y15c)
 print '----1.5 modo----'
-print 'Error at min:',(min(rom15)-min(y15o))/min(y15o)
-print 'Overall Error:',error
-print 'Overall Stand Dev:',errorstd
+if errortype == 'abs':
+    print '\n Average Absolute Error'
+elif errortype == 'rel':
+    print '\n Average Relative Error'
+elif errortype == 'rms':
+    print '\n Root Mean Squared Error'
+print '\nAverage Error:',error
+print 'Average Stand Dev:',errorstd
+low = y15o.min()
+lowrom = rom15.min()
+if errortype == 'abs' or errortype == 'rms':
+     mdferror = fabs((1.-lowrom)-(1.-low))
+elif errortype == 'rel':
+    mdferror = fabs(((1.-lowrom)-(1.-low))/(1.-low))
+print 'Maximum Deficit Error:',mdferror
+
 # plt.savefig('/Users/ning1/Documents/FLOW Lab/bat_val.png')
 
 plt.show()

wake_model/validation/tescione_val.py
@@ -2,9 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 from VAWT_Wake_Model import velocity_field
-from database_call import vorticity,vorticity2,velocity,velocity2
 from scipy.io import loadmat
-from numpy import fabs, log
+from numpy import fabs
 
 from matplotlib import rcParams
 rcParams['font.family'] = 'Times New Roman'
@@ -15,6 +14,10 @@ r = 0.5 # radius
 v = 1.0 # velocity
 
 veltype = 'x'
+errortype = 'abs'
+errortype = 'rel'
+epsilon = 1e-3
+errortype = 'rms'
 
 # Import Star-CCM+ simulation data (from validation study)
 # fdata = 'path/to/tes.csv' # adjust this for your case
@@ -133,27 +136,93 @@ cfd30t = np.zeros(33)
 cfd35t = np.zeros(33)
 cfd40t = np.zeros(26)
 
-for i in range(33):
-    cfd15t[i] = (vel1[int(index15[i])]-y15[i])/y15[i]
-    cfd20t[i] = (vel2[int(index20[i])]-y20[i])/y20[i]
-    cfd25t[i] = (vel3[int(index25[i])]-y25[i])/y25[i]
-    cfd30t[i] = (vel4[int(index30[i])]-y30[i])/y30[i]
-    cfd35t[i] = (vel5[int(index35[i])]-y35[i])/y35[i]
-for i in range(26):
-    cfd40t[i] = (vel6[int(index40[i])]-y40[i])/y40[i]
 
-cfd15error = np.average(fabs(cfd15t))
-cfd15errorstd = np.std(cfd15t)
-cfd20error = np.average(fabs(cfd20t))
-cfd20errorstd = np.std(cfd20t)
-cfd25error = np.average(fabs(cfd25t))
-cfd25errorstd = np.std(cfd25t)
-cfd30error = np.average(fabs(cfd30t))
-cfd30errorstd = np.std(cfd30t)
-cfd35error = np.average(fabs(cfd35t))
-cfd35errorstd = np.std(cfd35t)
-cfd40error = np.average(fabs(cfd40t))
-cfd40errorstd = np.std(cfd40t)
+for i in range(33):
+    if errortype == 'abs':
+        cfd15t[i] = fabs((1. - vel1[int(index15[i])])-(1. - y15[i]))
+    elif errortype == 'rel':
+        if fabs(1. - y15[i]) >= epsilon:
+            cfd15t[i] = fabs(((1. - vel1[int(index15[i])])-(1. - y15[i]))/(1. - y15[i]))
+        else:
+            cfd15t[i] = fabs((1. - vel1[int(index15[i])])-(1. - y15[i]))
+    elif errortype == 'rms':
+        cfd15t[i] = ((1. - vel1[int(index15[i])])-(1. - y15[i]))**2
+    if errortype == 'abs':
+        cfd20t[i] = fabs((1. - vel2[int(index20[i])])-(1. - y20[i]))
+    elif errortype == 'rel':
+        if fabs(1. - y20[i]) >= epsilon:
+            cfd20t[i] = fabs(((1. - vel2[int(index20[i])])-(1. - y20[i]))/(1. - y20[i]))
+        else:
+            cfd20t[i] = fabs((1. - vel2[int(index20[i])])-(1. - y20[i]))
+    elif errortype == 'rms':
+        cfd20t[i] = ((1. - vel2[int(index20[i])])-(1. - y20[i]))**2
+    if errortype == 'abs':
+        cfd25t[i] = fabs((1. - vel3[int(index25[i])])-(1. - y25[i]))
+    elif errortype == 'rel':
+        if fabs(1. - y25[i]) >= epsilon:
+            cfd25t[i] = fabs(((1. - vel3[int(index25[i])])-(1. - y25[i]))/(1. - y25[i]))
+        else:
+            cfd25t[i] = fabs((1. - vel3[int(index25[i])])-(1. - y25[i]))
+    elif errortype == 'rms':
+        cfd25t[i] = ((1. - vel3[int(index25[i])])-(1. - y25[i]))**2
+    if errortype == 'abs':
+        cfd30t[i] = fabs((1. - vel4[int(index30[i])])-(1. - y30[i]))
+    elif errortype == 'rel':
+        if fabs(1. - y30[i]) >= epsilon:
+            cfd30t[i] = fabs(((1. - vel4[int(index30[i])])-(1. - y30[i]))/(1. - y30[i]))
+        else:
+            cfd30t[i] = fabs((1. - vel4[int(index30[i])])-(1. - y30[i]))
+    elif errortype == 'rms':
+        cfd30t[i] = ((1. - vel4[int(index30[i])])-(1. - y30[i]))**2
+    if errortype == 'abs':
+        cfd35t[i] = fabs((1. - vel5[int(index35[i])])-(1. - y35[i]))
+    elif errortype == 'rel':
+        if fabs(1. - y35[i]) >= epsilon:
+            cfd35t[i] = fabs(((1. - vel5[int(index35[i])])-(1. - y35[i]))/(1. - y35[i]))
+        else:
+            cfd35t[i] = fabs((1. - vel5[int(index35[i])])-(1. - y35[i]))
+    elif errortype == 'rms':
+        cfd35t[i] = ((1. - vel5[int(index35[i])])-(1. - y35[i]))**2
+for i in range(26):
+    if errortype == 'abs':
+        cfd40t[i] = fabs((1. - vel6[int(index40[i])])-(1. - y40[i]))
+    elif errortype == 'rel':
+        if fabs(1. - y40[i]) >= epsilon:
+            cfd40t[i] = fabs(((1. - vel6[int(index40[i])])-(1. - y40[i]))/(1. - y40[i]))
+        else:
+            cfd40t[i] = fabs((1. - vel6[int(index40[i])])-(1. - y40[i]))
+    elif errortype == 'rms':
+        cfd40t[i] = ((1. - vel6[int(index40[i])])-(1. - y40[i]))**2
+
+
+
+if errortype == 'abs' or errortype == 'rel':
+    cfd15error = np.average(cfd15t)
+    cfd15errorstd = np.std(cfd15t)
+    cfd20error = np.average(cfd20t)
+    cfd20errorstd = np.std(cfd20t)
+    cfd25error = np.average(cfd25t)
+    cfd25errorstd = np.std(cfd25t)
+    cfd30error = np.average(cfd30t)
+    cfd30errorstd = np.std(cfd30t)
+    cfd35error = np.average(cfd35t)
+    cfd35errorstd = np.std(cfd35t)
+    cfd40error = np.average(cfd40t)
+    cfd40errorstd = np.std(cfd40t)
+elif errortype == 'rms':
+    cfd15error = np.sqrt(np.average(cfd15t))
+    cfd15errorstd = 1.
+    cfd20error = np.sqrt(np.average(cfd20t))
+    cfd20errorstd = 1.
+    cfd25error = np.sqrt(np.average(cfd25t))
+    cfd25errorstd = 1.
+    cfd30error = np.sqrt(np.average(cfd30t))
+    cfd30errorstd = 1.
+    cfd35error = np.sqrt(np.average(cfd35t))
+    cfd35errorstd = 1.
+    cfd40error = np.sqrt(np.average(cfd40t))
+    cfd40errorstd = 1.
+
 cfdoaerror = (cfd15error+cfd20error+cfd25error+cfd30error+cfd35error+cfd40error)/6.
 cfdoaerrorstd = (cfd15errorstd+cfd20errorstd+cfd25errorstd+cfd30errorstd+cfd35errorstd+cfd40errorstd)/6.
 
@@ -169,7 +238,7 @@ plt.ylim(0.1,1.2)
 plt.xlabel('$y/D$')
 plt.ylabel(r'$u/U_\infty$')
 plt.text(-0.25,1.05,r'$x/D$ = 0.75')
-print '1.5 cfd',(min(vel1)-min(y15))/min(y15),cfd15error,cfd15errorstd
+print '1.5 cfd',cfd15error,cfd15errorstd
 plt.subplot(2,3,2)
 plt.plot(x20,y20,'b.')
 plt.plot(pos2,vel2,'r-')
@@ -178,7 +247,7 @@ plt.ylim(0.1,1.2)
 plt.xlabel('$y/D$')
 plt.ylabel(r'$u/U_\infty$')
 plt.text(-0.25,1.05,r'$x/D$ = 1.0')
-print '2.0 cfd',(min(vel2)-min(y20))/min(y20),cfd20error,cfd20errorstd
+print '2.0 cfd',cfd20error,cfd20errorstd
 plt.subplot(2,3,3)
 plt.plot(x25,y25,'b.',label='PIV')
 plt.plot(pos3,vel3,'r-',label='CFD')
@@ -187,7 +256,7 @@ plt.ylim(0.1,1.2)
 plt.xlabel('$y/D$')
 plt.ylabel(r'$u/U_\infty$')
 plt.text(-0.25,1.05,r'$x/D$ = 1.25')
-print '2.5 cfd',(min(vel3)-min(y25))/min(y25),cfd25error,cfd25errorstd
+print '2.5 cfd',cfd25error,cfd25errorstd
 plt.legend(loc="upper left", bbox_to_anchor=(1,1))
 plt.subplot(2,3,4)
 plt.plot(x30,y30,'b.')
@@ -197,7 +266,7 @@ plt.ylim(0.1,1.2)
 plt.xlabel('$y/D$')
 plt.ylabel(r'$u/U_\infty$')
 plt.text(-0.25,1.05,r'$x/D$ = 1.5')
-print '3.0 cfd',(min(vel4)-min(y30))/min(y30),cfd30error,cfd30errorstd
+print '3.0 cfd',cfd30error,cfd30errorstd
 plt.subplot(2,3,5)
 plt.plot(x35,y35,'b.')
 plt.plot(pos5,vel5,'r-')
@@ -206,7 +275,7 @@ plt.ylim(0.1,1.2)
 plt.xlabel('$y/D$')
 plt.ylabel(r'$u/U_\infty$')
 plt.text(-0.25,1.05,r'$x/D$ = 1.75')
-print '3.5 cfd',(min(vel5)-min(y35))/min(y35),cfd35error,cfd35errorstd
+print '3.5 cfd',cfd35error,cfd35errorstd
 plt.subplot(2,3,6)
 plt.plot(x40,y40,'b.')
 plt.plot(pos6,vel6,'r-')
@@ -215,7 +284,7 @@ plt.ylim(0.1,1.2)
 plt.xlabel('$y/D$')
 plt.ylabel(r'$u/U_\infty$')
 plt.text(-0.25,1.05,r'$x/D$ = 2.0')
-print '4.0 cfd',(min(vel6)-min(y40))/min(y40),cfd40error,cfd40errorstd
+print '4.0 cfd',cfd40error,cfd40errorstd
 
 
 print '\nOverall Error:',cfdoaerror
@@ -250,36 +319,121 @@ if rom == True:
     rom30t = np.zeros(33)
     rom35t = np.zeros(33)
     rom40t = np.zeros(26)
+
+    # for i in range(33):
+    #     rom15[i] = velocity_field(xt,yt,0.75*dia,x15[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
+    #     rom15t[i] = (rom15[i]-y15[i])/y15[i]
+    #     rom20[i] = velocity_field(xt,yt,1.0*dia,x20[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
+    #     rom20t[i] = (rom20[i]-y20[i])/y20[i]
+    #     rom25[i] = velocity_field(xt,yt,1.25*dia,x25[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
+    #     rom25t[i] = (rom25[i]-y25[i])/y25[i]
+    #     rom30[i] = velocity_field(xt,yt,1.5*dia,x30[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
+    #     rom30t[i] = (rom30[i]-y30[i])/y30[i]
+    #     rom35[i] = velocity_field(xt,yt,1.75*dia,x35[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
+    #     rom35t[i] = (rom35[i]-y35[i])/y35[i]
+    #     rom40f[i] = velocity_field(xt,yt,2.0*dia,x35[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
+    #     print i
+    # for i in range(26):
+    #     rom40[i] = velocity_field(xt,yt,2.0*dia,x40[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
+    #     rom40t[i] = (rom40[i]-y40[i])/y40[i]
+    #     print i
+
     for i in range(33):
         rom15[i] = velocity_field(xt,yt,0.75*dia,x15[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
-        rom15t[i] = (rom15[i]-y15[i])/y15[i]
         rom20[i] = velocity_field(xt,yt,1.0*dia,x20[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
-        rom20t[i] = (rom20[i]-y20[i])/y20[i]
         rom25[i] = velocity_field(xt,yt,1.25*dia,x25[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
-        rom25t[i] = (rom25[i]-y25[i])/y25[i]
         rom30[i] = velocity_field(xt,yt,1.5*dia,x30[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
-        rom30t[i] = (rom30[i]-y30[i])/y30[i]
         rom35[i] = velocity_field(xt,yt,1.75*dia,x35[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
-        rom35t[i] = (rom35[i]-y35[i])/y35[i]
         rom40f[i] = velocity_field(xt,yt,2.0*dia,x35[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
         print i
     for i in range(26):
         rom40[i] = velocity_field(xt,yt,2.0*dia,x40[i]*dia,velf,dia,rot,chord,B,param=None,veltype=veltype)
-        rom40t[i] = (rom40[i]-y40[i])/y40[i]
         print i
 
-    rom15error = np.average(fabs(rom15t))
-    rom15errorstd = np.std(fabs(rom15t))
-    rom20error = np.average(fabs(rom20t))
-    rom20errorstd = np.std(fabs(rom20t))
-    rom25error = np.average(fabs(rom25t))
-    rom25errorstd = np.std(fabs(rom25t))
-    rom30error = np.average(fabs(rom30t))
-    rom30errorstd = np.std(fabs(rom30t))
-    rom35error = np.average(fabs(rom35t))
-    rom35errorstd = np.std(fabs(rom35t))
-    rom40error = np.average(fabs(rom40t))
-    rom40errorstd = np.std(fabs(rom40t))
+    for i in range(33):
+        if errortype == 'abs':
+            rom15t[i] = fabs((1. - rom15[i])-(1. - y15[i]))
+        elif errortype == 'rel':
+            if fabs(1. - y15[i]) >= epsilon:
+                rom15t[i] = fabs(((1. - rom15[i])-(1. - y15[i]))/(1. - y15[i]))
+            else:
+                rom15t[i] = fabs((1. - rom15[i])-(1. - y15[i]))
+        elif errortype == 'rms':
+            rom15t[i] = ((1. - rom15[i])-(1. - y15[i]))**2.
+        if errortype == 'abs':
+            rom20t[i] = fabs((1. - rom20[i])-(1. - y20[i]))
+        elif errortype == 'rel':
+            if fabs(1. - y20[i]) >= epsilon:
+                rom20t[i] = fabs(((1. - rom20[i])-(1. - y20[i]))/(1. - y20[i]))
+            else:
+                rom20t[i] = fabs((1. - rom20[i])-(1. - y20[i]))
+        elif errortype == 'rms':
+            rom20t[i] = ((1. - rom20[i])-(1. - y20[i]))**2.
+        if errortype == 'abs':
+            rom25t[i] = fabs((1. - rom25[i])-(1. - y25[i]))
+        elif errortype == 'rel':
+            if fabs(1. - y25[i]) >= epsilon:
+                rom25t[i] = fabs(((1. - rom25[i])-(1. - y25[i]))/(1. - y25[i]))
+            else:
+                rom25t[i] = fabs((1. - rom25[i])-(1. - y25[i]))
+        elif errortype == 'rms':
+            rom25t[i] = ((1. - rom25[i])-(1. - y25[i]))**2.
+        if errortype == 'abs':
+            rom30t[i] = fabs((1. - rom30[i])-(1. - y30[i]))
+        elif errortype == 'rel':
+            if fabs(1. - y30[i]) >= epsilon:
+                rom30t[i] = fabs(((1. - rom30[i])-(1. - y30[i]))/(1. - y30[i]))
+            else:
+                rom30t[i] = fabs((1. - rom30[i])-(1. - y30[i]))
+        elif errortype == 'rms':
+            rom30t[i] = ((1. - rom30[i])-(1. - y30[i]))**2.
+        if errortype == 'abs':
+            rom35t[i] = fabs((1. - rom35[i])-(1. - y35[i]))
+        elif errortype == 'rel':
+            if fabs(1. - y35[i]) >= epsilon:
+                rom35t[i] = fabs(((1. - rom35[i])-(1. - y35[i]))/(1. - y35[i]))
+            else:
+                rom35t[i] = fabs((1. - rom35[i])-(1. - y35[i]))
+        elif errortype == 'rms':
+            rom35t[i] = ((1. - rom35[i])-(1. - y35[i]))**2.
+    for i in range(26):
+        if errortype == 'abs':
+            rom40t[i] = fabs((1. - rom40[i])-(1. - y40[i]))
+        elif errortype == 'rel':
+            if fabs(1. - y40[i]) >= epsilon:
+                rom40t[i] = fabs(((1. - rom40[i])-(1. - y40[i]))/(1. - y40[i]))
+            else:
+                rom40t[i] = fabs((1. - rom40[i])-(1. - y40[i]))
+        elif errortype == 'rms':
+            rom40t[i] = ((1. - rom40[i])-(1. - y40[i]))**2.
+
+
+    if errortype == 'abs' or errortype == 'rel':
+        rom15error = np.average(rom15t)
+        rom15errorstd = np.std(rom15t)
+        rom20error = np.average(rom20t)
+        rom20errorstd = np.std(rom20t)
+        rom25error = np.average(rom25t)
+        rom25errorstd = np.std(rom25t)
+        rom30error = np.average(rom30t)
+        rom30errorstd = np.std(rom30t)
+        rom35error = np.average(rom35t)
+        rom35errorstd = np.std(rom35t)
+        rom40error = np.average(rom40t)
+        rom40errorstd = np.std(rom40t)
+    elif errortype == 'rms':
+        rom15error = np.sqrt(np.average(rom15t))
+        rom15errorstd = 1.
+        rom20error = np.sqrt(np.average(rom20t))
+        rom20errorstd = 1.
+        rom25error = np.sqrt(np.average(rom25t))
+        rom25errorstd = 1.
+        rom30error = np.sqrt(np.average(rom30t))
+        rom30errorstd = 1.
+        rom35error = np.sqrt(np.average(rom35t))
+        rom35errorstd = 1.
+        rom40error = np.sqrt(np.average(rom40t))
+        rom40errorstd = 1.
     oaerror = (rom15error+rom20error+rom25error+rom30error+rom35error+rom40error)/6.
     oaerrorstd = (rom15errorstd+rom20errorstd+rom25errorstd+rom30errorstd+rom35errorstd+rom40errorstd)/6.
 
@@ -405,42 +559,70 @@ if rom == True:
     print '4.0 mod',(min(rom40f)-min(y40))/min(y40),rom40error,rom40errorstd
     print '4.0 cfd',(min(vel6)-min(y40))/min(y40),cfd40error,cfd40errorstd
 
-    print '\nOverall Error:',oaerror
-    print 'Overall Stand Dev:',oaerrorstd
+
+    if errortype == 'abs':
+        print '\n Average Absolute Error'
+    elif errortype == 'rel':
+        print '\n Average Relative Error'
+    elif errortype == 'rms':
+        print '\n Root Mean Squared Error'
+    print '\nAverage CFD Error:',cfdoaerror
+    print 'Average CFD Stand Dev:',cfdoaerrorstd
+    print '\nAverage ROM Error:',oaerror
+    print 'Average ROM Stand Dev:',oaerrorstd
+
+    low15 = y15.min()
+    low20 = y20.min()
+    low25 = y25.min()
+    low30 = y30.min()
+    low35 = y35.min()
+    low40 = y40.min()
+    low15cfd = vel1.min()
+    low20cfd = vel2.min()
+    low25cfd = vel3.min()
+    low30cfd = vel4.min()
+    low35cfd = vel5.min()
+    low40cfd = vel6.min()
+    low15rom = rom15.min()
+    low20rom = rom20.min()
+    low25rom = rom25.min()
+    low30rom = rom30.min()
+    low35rom = rom35.min()
+    low40rom = rom40.min()
+
+    if errortype == 'abs' or errortype == 'rms':
+        mdfcfd15 = fabs((1.-low15cfd)-(1.-low15))
+        mdfcfd20 = fabs((1.-low20cfd)-(1.-low20))
+        mdfcfd25 = fabs((1.-low25cfd)-(1.-low25))
+        mdfcfd30 = fabs((1.-low30cfd)-(1.-low30))
+        mdfcfd35 = fabs((1.-low35cfd)-(1.-low35))
+        mdfcfd40 = fabs((1.-low40cfd)-(1.-low40))
+
+        mdfrom15 = fabs((1.-low15rom)-(1.-low15))
+        mdfrom20 = fabs((1.-low20rom)-(1.-low20))
+        mdfrom25 = fabs((1.-low25rom)-(1.-low25))
+        mdfrom30 = fabs((1.-low30rom)-(1.-low30))
+        mdfrom35 = fabs((1.-low35rom)-(1.-low35))
+        mdfrom40 = fabs((1.-low40rom)-(1.-low40))
+
+    elif errortype == 'rel':
+        mdfcfd15 = fabs(((1.-low15cfd)-(1.-low15))/(1.-low15))
+        mdfcfd20 = fabs(((1.-low20cfd)-(1.-low20))/(1.-low20))
+        mdfcfd25 = fabs(((1.-low25cfd)-(1.-low25))/(1.-low25))
+        mdfcfd30 = fabs(((1.-low30cfd)-(1.-low30))/(1.-low30))
+        mdfcfd35 = fabs(((1.-low35cfd)-(1.-low35))/(1.-low35))
+        mdfcfd40 = fabs(((1.-low40cfd)-(1.-low40))/(1.-low40))
+
+        mdfrom15 = fabs(((1.-low15rom)-(1.-low15))/(1.-low15))
+        mdfrom20 = fabs(((1.-low20rom)-(1.-low20))/(1.-low20))
+        mdfrom25 = fabs(((1.-low25rom)-(1.-low25))/(1.-low25))
+        mdfrom30 = fabs(((1.-low30rom)-(1.-low30))/(1.-low30))
+        mdfrom35 = fabs(((1.-low35rom)-(1.-low35))/(1.-low35))
+        mdfrom40 = fabs(((1.-low40rom)-(1.-low40))/(1.-low40))
+
+    print 'Maximum Deficit Error (CFD):',mdfcfd15,mdfcfd20,mdfcfd25,mdfcfd30,mdfcfd35,mdfcfd40,'\n\t',max(mdfcfd15,mdfcfd20,mdfcfd25,mdfcfd30,mdfcfd35,mdfcfd40)
+    print 'Maximum Deficit Error (ROM):',mdfrom15,mdfrom20,mdfrom25,mdfrom30,mdfrom35,mdfrom40,'\n\t',max(mdfrom15,mdfrom20,mdfrom25,mdfrom30,mdfrom35,mdfrom40)
+
     
 plt.show()
-
-# CFD
-# 1.5 cfd 0.085222697309 0.126583824064 0.145411541539
-# 2.0 cfd 0.100156455326 0.128983354685 0.153469587078
-# 2.5 cfd 0.157759607312 0.150857817984 0.170220943027
-# 3.0 cfd 0.229735606591 0.185110325841 0.202881236867
-# 3.5 cfd 0.221745704619 0.221675365171 0.241148027704
-# 4.0 cfd 0.229931173793 0.229559158614 0.230378847745
-# 0.173794974393 0.19058503066
-# Vort
-# 1.5 mod 0.132406448951 0.123814266617 0.0552752953268
-# 2.0 mod 0.0899133305592 0.144327259244 0.0820073765436
-# 2.5 mod 0.121791964707 0.168153726068 0.1329647045
-# 3.0 mod 0.18331069938 0.199964293136 0.162818805975
-# 3.5 mod 0.178599395018 0.243762270671 0.204391618207
-# 4.0 mod 0.195484403221 0.227921962845 0.158527848792
-# 0.18465729643 0.132664274891
-# Velo- val
-# 1.5 mod 0.0764360005636 0.130154973392 0.107578354084
-# 2.0 mod 0.114032956588 0.162965148463 0.11913931666
-# 2.5 mod 0.19796690214 0.207943768574 0.159908756532
-# 3.0 mod 0.296349051996 0.256526949057 0.213454303636
-# 3.5 mod 0.309388725898 0.310579829187 0.2733377453
-# 4.0 mod 0.337190466884 0.230182235911 0.132586893453
-# 0.216392150764 0.167667561611
-# Velo- flip
-# 1.5 mod 0.0768091662811 0.0653944355885 0.0726633578795
-# 2.0 mod 0.114357543787 0.0787570867999 0.0793591266985
-# 2.5 mod 0.198250408813 0.12999380938 0.105127979898
-# 3.0 mod 0.296586892386 0.174819197659 0.15220512067
-# 3.5 mod 0.309562485502 0.226642726267 0.207679397223
-# 4.0 mod 0.337304393801 0.174324433215 0.139728360992
-# 0.141655281485 0.126127223893
-
 