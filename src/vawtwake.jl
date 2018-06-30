369cb1b9f8680e7f8dc3e42b012363b84100d259
Dagan <dagan.pielstick@gmail.com>
2018-06-29 18:24:50 -0600

    Updated to work with vawtac module

--- src/vawtwake.jl
+++ src/vawtwake.jl
@@ -2,7 +2,8 @@ module vawtwake
 using SpecialFunctions
 using QuadGK
 using CSV
-export vfieldx,vfieldy,vorticitystrength,model_gen,velocity_field
+using Dierckx
+export vfieldx,vfieldy,vorticitystrength,model_gen,velocity_field,overlap,airfoil_data
 path,_ = splitdir(@__FILE__)
 
 struct Arguments
@@ -236,7 +237,7 @@ end #vfieldy
 function model_gen(x0,y0,tsr,solidity,dia,rot)
     #Get file locatoin (absolute)
     #path,_ = splitdir(@__FILE__)
-    fileloc="$path/../Data/VAWTPolySurfaceCoef_pub.csv" #Not sure if this is the best way
+    fileloc="$path/../data/VAWTPolySurfaceCoef_pub.csv" #Not sure if this is the best way
     csvdata=CSV.read(fileloc,delim=',')
     #Project Point Onto Surface
     loc1=_parameterval(tsr,solidity,csvdata[:,1])
@@ -317,5 +318,172 @@ function _parameterval(tsr,sol,coef)
     surf = a + b*tsr + c*sol + d*tsr^2 + e*tsr*sol + f*sol^2 + g*tsr^3 + h*tsr^2*sol + i*tsr*sol^2 + j*sol^3
     return surf
 end #_parameterval
+
+function pvawt(p,xt,yt,xd,yd,Vinf,diat,rott,chord,B,veltype,param)
+        a=zeros(p,2)
+        for k=1:p
+                wake=vawtwake.velocity_field(xt,yt,xd[k],yd[k],Vinf,diat,rott,chord,B,veltype,param)
+                a[k,1]=wake[1]
+                a[k,2]=wake[2]
+        end
+        return a
+end
+
+function overlap(p,xt,yt,diat,rott,chord,B,x0,y0,dia,Vinf,pointcalc,param="None",veltype="ind")
+        np=nprocs()
+        t=length(xt)
+        xd=zeros(p)
+        yd=zeros(p)
+        velx=zeros(p)
+        vely=zeros(p)
+        velx_int=zeros(p)
+        vely_int=zeros(p)
+        #Find points around the flight path of the blades
+
+        for i=1:p
+                if pointcalc==false
+                        theta=(2*pi/p)*i-(2*pi/p)/2
+                        xd[i]=x0-sin(theta)*(dia[1]/2.0)
+                        yd[i]=y0+cos(theta)*(dia[1]/2.0)
+                elseif pointcalc==true
+                        xd[1]=0
+                        yd[1]=0
+                end
+        end
+        intex=zeros(p)
+        intey=zeros(p)
+
+        if t==1 #Coupled Confiugration
+                if pointcalc==false
+                        for j=1:p
+                                wake=velocity_field(xt[1],yt[1],xd[j],yd[j],Vinf,diat[1],chord,B,veltype,param)
+                                velx[j]=wake[1]*Vinf
+                                vely[j]=wake[2]*Vinf
+                        end
+                elseif pointcalc==true
+                        wake = velocity_field(xt[1],yt[1],xd[1],yd[1],Vinf,diat[1],rott[1],chord,B,veltype,param)
+                        velx[1] = wake[1]*Vinf
+                        vely[1] = wake[2]*Vinf
+                end
+        else
+                if pointcalc==false
+                        if np==1
+                                for j=1:t
+                                        for k=1:p
+                                                wake=velocity_field(xt[j],yt[j],xd[k],yd[k],Vinf,diat[j],rott[j],chord,B,veltype,param)
+                                                velx_int[k] = -wake[1]
+                                                vely_int[k] = wake[2]
+                                                if (velx_int[k] >= 0.0)
+                                                        intex[k] = intex[k] + (velx_int[k])^2
+                                                else
+                                                        intex[k] = intex[k] - (velx_int[k])^2
+                                                end
+                                                if (vely_int[k] >= 0.0)
+                                                        intey[k] = intey[k] + (vely_int[k])^2
+                                                else
+                                                        intey[k] = intey[k] - (vely_int[k])^2
+                                                end
+                                        end
+                                end
+                        elseif np>1
+                                wake=zeros(t,p,2)
+                                #Do the Calculation First
+                                wake=@parallel vcat for j=1:t
+                                    pvawt(p,xt[j],yt[j],xd,yd,Vinf,diat[j],rott[j],chord,B,veltype,param)
+                                end
+                                #Then Perform numbers
+                                for j=1:t
+                                        for k=1:p
+                                                velx_int[1] = -wake[(j-1)*p+k,1]
+                                                vely_int[1] = wake[(j-1)*p+k,2]
+
+                                                # sum of squares of velocity deficits
+                                                if (velx_int[1] >= 0.0)
+                                                        intex[1] = intex[1] + (velx_int[1])^2
+                                                else
+                                                        intex[1] = intex[1] - (velx_int[1])^2
+                                                end
+                                                if (vely_int[1] >= 0.0)
+                                                        intey[1] = intey[1] + (vely_int[1])^2
+                                                else
+                                                        intey[1] = intey[1] - (vely_int[1])^2
+                                                end
+                                        end
+                                end
+
+                        end
+        elseif pointcalc==true
+                        for j=1:t
+                                wake = velocity_field(xt[j],yt[j],xd[1],yd[1],Vinf,diat[j],rott[j],chord,B,veltype,param)
+                                velx_int[1] = -wake[1]
+                                vely_int[1] = wake[2]
+
+                                # sum of squares of velocity deficits
+                                if (velx_int[1] >= 0.0)
+                                        intex[1] = intex[1] + (velx_int[1])^2
+                                else
+                                        intex[1] = intex[1] - (velx_int[1])^2
+                                end
+                                if (vely_int[1] >= 0.0)
+                                        intey[1] = intey[1] + (vely_int[1])^2
+                                else
+                                        intey[1] = intey[1] - (vely_int[1])^2
+                                end
+                        end
+                end
+                #square root of sum squares
+                for l=1:p
+                        if (intex[l] >= 0.0)
+                                velx[l] = -Vinf*(sqrt(intex[l]))
+                        else
+                                velx[l] = Vinf*(sqrt(abs(intex[l])))
+                        end
+                        if (intey[l] >= 0.0)
+                                vely[l] = Vinf*(sqrt(intey[l]))
+                        else
+                                vely[l] = -Vinf*(sqrt(abs(intey[l])))
+                        end
+                end
+        end
+        return velx,vely
+end #overlap
+
+function airfoil_data(file)
+        fileloc="$path/../data/airfoils/$file"
+        af_data=Array{Float64}(0)
+        cl_data=Array{Float64}(0)
+        cd_data=Array{Float64}(0)
+        open(fileloc) do f
+                for i in enumerate(eachline(f))
+                        count=i[1]
+                        str=i[2]
+                        if count>13
+                                line=split(str)
+                                if line[1]=="EOT"
+
+                                else
+                                        af_data=append!(af_data,float(line[1]))
+                                        cl_data=append!(cl_data,float(line[2]))
+                                        cd_data=append!(cd_data,float(line[3]))
+                                end
+                        end
+                end
+        end
+        return af_data,cl_data,cd_data
+        #Smoothing data with a spline
+
+
+        clsmooth=Dierckx.Spline1D(af_data,cl_data,s=0.1)
+        cdsmooth=Dierckx.Spline1D(af_data,cd_data,s=0.001)
+
+        #Reassigning imported airfoild data with the smoothed airfoil data
+        af_data_smooth = linspace(af_data[0],af_data[end],4000)
+        cl_data_smooth = evaluate(clsmooth,af_data_smooth)
+        cd_data_smooth = evaluate(cdsmooth,af_data_smooth)
+
+        #Return Smoothed data
+        return af_data_smooth,cl_data_smooth,cd_data_smooth
+end #airfoil data
+
 
 end #module
